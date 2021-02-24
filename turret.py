298b2c80a9e739fb2441f34b4e2ae982b14c0e51
aaluong <60415655+aaluong@users.noreply.github.com>
2021-02-24 10:12:36 -0800

    Update turret.py

--- turret.py
+++ turret.py
@@ -137,9 +137,9 @@ class VideoUtils(object):
                 # compute the bounding box for the contour, draw it on the frame,
                 # and update the text
                 (x, y, w, h) = cv2.boundingRect(c)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                callback(c, frame)
-		Repeat.motion_detection(show_video=true)
+                if cv2.countourArea(c) < 6000
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    callback(c, frame)
 
             # show the frame and record if the user presses a key
             if show_video:
