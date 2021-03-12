10c7641b6f63c5acd2af7ac42a540ce70585f724
aaluong <60415655+aaluong@users.noreply.github.com>
2021-03-12 11:10:20 -0800

    Update turret.py

--- turret.py
+++ turret.py
@@ -22,8 +22,8 @@ from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_Step
 MOTOR_X_REVERSED = False
 MOTOR_Y_REVERSED = False
 
-MAX_STEPS_X = 5
-MAX_STEPS_Y = 5
+MAX_STEPS_X = 15
+MAX_STEPS_Y = 10
 
 RELAY_PIN = 22
 
